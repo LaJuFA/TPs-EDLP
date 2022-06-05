e806a3183db4b36faf8adab1c2a5ff3729c7d37c
LaJuFA <104035802+LaJuFA@users.noreply.github.com>
2022-06-05 19:09:33 -0300

    Add files via upload

---
+++ TP 8/JS/script.js
@@ -0,0 +1,14 @@
+function NocFunction() {
+  document.body.style.backgroundColor = "#404040";
+  document.getElementById("SubTitulo").style.backgroundColor = "#333333";
+  document.getElementById("Header").style.backgroundColor = "#999966";
+  document.getElementById("Footer").style.backgroundColor = "#996633";
+  document.getElementById("Tex").style.backgroundColor = "#333333";
+  document.getElementById("Tex").style.color = "white";
+  document.getElementById("Tex2").style.backgroundColor = "#333333";
+  document.getElementById("Tex2").style.color = "white";
+}
+
+function Datos() {
+  alert("Autor : Lautaro Acuña");
+}
